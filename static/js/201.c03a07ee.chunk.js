277d9330a60c62bd561de026cfe6f16d22dabf3c
Narek-19 <narekchakhoyan97@gmail.com>
2023-07-24 22:34:15 +0400

    Updates

---
+++ static/js/201.c03a07ee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklesson_2_narek_chakhoyan=self.webpackChunklesson_2_narek_chakhoyan||[]).push([[201],{201:function(n,a,e){e.r(a),e.d(a,{default:function(){return c}});e(791);var s="image_imgContainer__UFIxn",r=e(184),c=function(n){var a=n.src,e=n.alt;return(0,r.jsx)("div",{className:s,children:(0,r.jsx)("img",{alt:e,src:a})})}}}]);
+//# sourceMappingURL=201.c03a07ee.chunk.js.map
